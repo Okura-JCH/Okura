6236c5b4aa570fa7b21ae8ee997ee92ec3ae4167
Hina Nakahira <soleil.colza@icloud.com>
2024-03-18 01:58:17 +0900

    [add]modules

--- app/src/main/java/com/example/okura/presentation/view/HomeScreen.kt
+++ app/src/main/java/com/example/okura/presentation/view/HomeScreen.kt
@@ -3,7 +3,8 @@ package com.example.okura.presentation.view
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
+
+
 @Composable
 fun HomeScreen(navController: NavHostController) {
-
 }
