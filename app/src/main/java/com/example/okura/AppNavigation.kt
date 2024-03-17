75bb5cacd78faf1e01a66b4626d29a2f42b6208b
Hina Nakahira <soleil.colza@icloud.com>
2024-03-18 01:28:00 +0900

    [add]spaces very end

--- app/src/main/java/com/example/okura/AppNavigation.kt
+++ app/src/main/java/com/example/okura/AppNavigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.okura.presentation.view.SearchArticleScreen
 
 @Composable
 fun AppNavigation() {
@@ -15,3 +16,6 @@ fun AppNavigation() {
         }
     }
 }
+
+
+
