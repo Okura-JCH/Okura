ab969da91cfbb88ded9475d432c38b1120033ee7
Hina Nakahira <soleil.colza@icloud.com>
2024-03-18 22:26:20 +0900

    なんかローカルのGit履歴変なことなってたので復元

--- app/src/main/java/com/example/okura/MainActivity.kt
+++ app/src/main/java/com/example/okura/MainActivity.kt
@@ -6,9 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 import com.example.okura.ui.theme.OkuraTheme
 
 class MainActivity : ComponentActivity() {
