8c7d6cbe8094a0097411fd1d54999477758b4c3d
Hina Nakahira <soleil.colza@icloud.com>
2024-03-19 02:26:31 +0900

    [add]HomeNavBar

---
+++ app/src/main/java/com/example/okura/ui/components/HomeNavBar.kt
@@ -0,0 +1,29 @@
+package com.example.okura.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun HomeNavBar(){
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items = listOf("Home", "Articles", "People", "Settings")
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }
+            )
+        }
+    }
+}
