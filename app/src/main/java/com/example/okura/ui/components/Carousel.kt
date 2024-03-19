46eba42c04b0bc5854a320774dcc85f148b2da05
Hina Nakahira <soleil.colza@icloud.com>
2024-03-20 02:01:39 +0900

    [remove]Accompanist

---
+++ app/src/main/java/com/example/okura/ui/components/Carousel.kt
@@ -0,0 +1,8 @@
+package com.example.okura.ui.components
+
+import android.graphics.drawable.GradientDrawable
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
---
+++ app/src/main/java/com/example/okura/ui/components/CarouselCard.kt
@@ -0,0 +1,40 @@
+package com.example.okura.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.okura.R
+
+@Composable
+fun CarouselCard(){
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .size(width = 240.dp, height = 100.dp)
+    ) {
+        Image(painter = painterResource(id = R.drawable.sample), contentDescription = "sample")
+        Text(
+            text = "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CarouselCardPreview() {
+    CarouselCard()
+}
