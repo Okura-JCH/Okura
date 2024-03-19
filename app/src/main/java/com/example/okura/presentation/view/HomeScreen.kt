a82983774a9d845e1ab215235281ce845377175c
Hina Nakahira <soleil.colza@icloud.com>
2024-03-20 05:36:30 +0900

    [remove]Unused imports

--- app/src/main/java/com/example/okura/presentation/view/HomeScreen.kt
+++ app/src/main/java/com/example/okura/presentation/view/HomeScreen.kt
@@ -2,8 +2,6 @@ package com.example.okura.presentation.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
@@ -21,7 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.okura.ui.components.carousel.ArticleCarousel
