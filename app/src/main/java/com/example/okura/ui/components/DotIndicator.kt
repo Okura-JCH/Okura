2e4f131a2bc75cbc2e81fe749c6f39200a947029
Hina Nakahira <soleil.colza@icloud.com>
2024-03-20 05:14:17 +0900

    [add]DotIndicator

---
+++ app/src/main/java/com/example/okura/ui/components/DotIndicator.kt
@@ -0,0 +1,45 @@
+package com.example.okura.ui.components
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.ui.draw.clip
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun DotIndicators(
+    pagerState: PagerState,
+    pageCount: Int,
+    modifier: Modifier = Modifier,
+    selectedColor: Color = MaterialTheme.colorScheme.primary,
+    unselectedColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.3f)
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        repeat(pageCount) { index ->
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = 4.dp)
+                    .size(width = 8.dp, height = 8.dp)
+                    .clip(CircleShape)
+                    .background(if (pagerState.currentPage == index) selectedColor else unselectedColor)
+            )
+        }
+    }
+}
