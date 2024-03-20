e17aeca69cf80ca94c7c555e1a472680bba86730
Hina Nakahira <soleil.colza@icloud.com>
2024-03-20 19:36:36 +0900

    [add]NewsFeed

---
+++ app/src/main/java/com/example/okura/presentation/components/feed/NewsFeed.kt
@@ -0,0 +1,27 @@
+package com.example.okura.presentation.components.feed
+
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.okura.domain.data.newsList
+import com.example.okura.domain.model.NewsItem
+
+@Composable
+fun NewsFeed(newsItems: List<NewsItem>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2)
+    ) {
+        items(newsItems) { newsItem ->
+            NewsCard(item = newsItem)
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewNewsFeed() {
+    NewsFeed(newsList)
+}
