0b3e180f1d51a9521301438e08f750742965c58f
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 00:46:55 +0900

    Added CategoryGrid

---
+++ app/src/main/java/com/example/okura/presentation/components/CategoryGrid.kt
@@ -0,0 +1,27 @@
+package com.example.okura.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.okura.domain.model.NewsCategory
+
+@Composable
+fun CategoryGrid(modifier: Modifier = Modifier) {
+    val categories = NewsCategory.newsCategories
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(categories) { category ->
+            CategoryCard(category = category)
+        }
+    }
+}
