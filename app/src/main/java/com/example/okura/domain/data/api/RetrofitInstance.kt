346230e9bf79d360f57a235b06e1a324c45b616c
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 02:39:12 +0900

    baseUrl modif

--- app/src/main/java/com/example/okura/domain/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/okura/domain/data/api/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("http://localhost:3000/")
+            .baseUrl("http://10.0.2.2:3000/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
