d39e6f176272bc8b8a4596232941ecaf91ab4f2d
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 02:37:06 +0900

    Removed sampleData newsList

--- app/src/main/java/com/example/okura/domain/data/repository/NewsRepository.kt
+++ app/src/main/java/com/example/okura/domain/data/repository/NewsRepository.kt
@@ -1,14 +1,10 @@
 package com.example.okura.domain.data.repository
 
+import com.example.okura.domain.data.api.RetrofitInstance
 import com.example.okura.domain.model.NewsItem
 
-val newsList =
-    List(40) {
-        NewsItem(
-            id = it,
-            categoryId = it,
-            title = "$it",
-            description = "$it",
-            imageUrl = "https://picsum.photos/200/300?random=$it",
-        )
+class NewsRepository {
+    suspend fun getArticles(): List<NewsItem> {
+        return RetrofitInstance.api.getArticles()
     }
+}
