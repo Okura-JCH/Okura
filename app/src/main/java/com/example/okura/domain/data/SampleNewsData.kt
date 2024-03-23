b2344bab8ff390211a5c1fde9eb73c80651185d7
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 00:26:30 +0900

    Added needed params

--- app/src/main/java/com/example/okura/domain/data/SampleNewsData.kt
+++ app/src/main/java/com/example/okura/domain/data/SampleNewsData.kt
@@ -7,12 +7,14 @@ val sampleNewsItems =
         NewsItem(
             id = 1,
             title = "Title",
+            categoryId = 1,
             description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
             imageUrl = "https://picsum.photos/200/300?random=1",
         ),
         NewsItem(
             id = 2,
             title = "Title",
+            categoryId = 2,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -22,6 +24,7 @@ val sampleNewsItems =
         NewsItem(
             id = 3,
             title = "Title",
+            categoryId = 3,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -31,6 +34,7 @@ val sampleNewsItems =
         NewsItem(
             id = 4,
             title = "Title",
+            categoryId = 4,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -40,6 +44,7 @@ val sampleNewsItems =
         NewsItem(
             id = 5,
             title = "Title",
+            categoryId = 5,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -49,6 +54,7 @@ val sampleNewsItems =
         NewsItem(
             id = 6,
             title = "Title",
+            categoryId = 6,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -58,6 +64,7 @@ val sampleNewsItems =
         NewsItem(
             id = 7,
             title = "Title",
+            categoryId = 7,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -67,6 +74,7 @@ val sampleNewsItems =
         NewsItem(
             id = 8,
             title = "Title",
+            categoryId = 8,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -76,6 +84,7 @@ val sampleNewsItems =
         NewsItem(
             id = 9,
             title = "Title",
+            categoryId = 9,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -85,6 +94,7 @@ val sampleNewsItems =
         NewsItem(
             id = 10,
             title = "Title",
+            categoryId = 10,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -94,6 +104,7 @@ val sampleNewsItems =
         NewsItem(
             id = 11,
             title = "Title",
+            categoryId = 11,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -103,6 +114,7 @@ val sampleNewsItems =
         NewsItem(
             id = 12,
             title = "Title",
+            categoryId = 12,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -112,6 +124,7 @@ val sampleNewsItems =
         NewsItem(
             id = 13,
             title = "Title",
+            categoryId = 13,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -121,6 +134,7 @@ val sampleNewsItems =
         NewsItem(
             id = 14,
             title = "Title",
+            categoryId = 14,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -130,6 +144,7 @@ val sampleNewsItems =
         NewsItem(
             id = 15,
             title = "Title",
+            categoryId = 15,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -139,6 +154,7 @@ val sampleNewsItems =
         NewsItem(
             id = 16,
             title = "Title",
+            categoryId = 16,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -148,6 +164,7 @@ val sampleNewsItems =
         NewsItem(
             id = 17,
             title = "Title",
+            categoryId = 17,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
@@ -157,6 +174,7 @@ val sampleNewsItems =
         NewsItem(
             id = 18,
             title = "Title",
+            categoryId = 18,
             description =
                 "Lorem ipsum dolor sit amet, consectetur " +
                     "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
