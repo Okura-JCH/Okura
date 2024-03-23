eef46d4470c0a65c9b57ef8c2e5bec56d59186c0
Hina Nakahira <soleil.colza@icloud.com>
2024-03-23 15:07:45 +0900

    [cassata]NavBar boilerplate setup

---
+++ app/src/main/java/com/example/okura/presentation/components/BottomNavBar.kt
@@ -0,0 +1,62 @@
+package com.example.okura.presentation.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun NavBar(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+) {
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items =
+        listOf(
+            "Home",
+            "Articles",
+            "People",
+            "Settings",
+        )
+
+    NavigationBar(modifier = modifier) {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Home, contentDescription = item) },
+                label = { Text("home") },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index },
+            )
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Search, contentDescription = item) },
+                label = { Text("search") },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index },
+            )
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Person, contentDescription = item) },
+                label = { Text("person") },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index },
+            )
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Settings, contentDescription = item) },
+                label = { Text("settings") },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index },
+            )
+        }
+    }
+}
