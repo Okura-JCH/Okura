99829c13981835372d117abab08e9b2efb0b5e31
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 02:17:41 +0900

    RetrofitInstance def

---
+++ app/src/main/java/com/example/okura/domain/data/api/RetrofitInstance.kt
@@ -0,0 +1,17 @@
+package com.example.okura.domain.data.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("http://localhost:3000/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val api: NewsApiService by lazy {
+        retrofit.create(NewsApiService::class.java)
+    }
+}
