d6fde59299185d590b547951abb6611b6effb087
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 03:41:20 +0900

    Added logger and httpClient

--- app/src/main/java/com/example/okura/domain/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/okura/domain/data/api/RetrofitInstance.kt
@@ -1,12 +1,24 @@
 package com.example.okura.domain.data.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
+val logger: HttpLoggingInterceptor =
+    HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+val httpClient: OkHttpClient =
+    OkHttpClient.Builder()
+        .addInterceptor(logger)
+        .build()
 
 object RetrofitInstance {
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:3000/")
+            .baseUrl("http://192.168.155.153:3000/")
+            .client(httpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
