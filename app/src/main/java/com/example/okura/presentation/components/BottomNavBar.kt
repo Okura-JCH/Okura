0a30376b8f834ac300d83893aa6bc347fe945597
Hina Nakahira <soleil.colza@icloud.com>
2024-03-23 15:13:59 +0900

    [add]BottomNavBar Preview

--- app/src/main/java/com/example/okura/presentation/components/BottomNavBar.kt
+++ app/src/main/java/com/example/okura/presentation/components/BottomNavBar.kt
@@ -15,7 +15,9 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun NavBar(
@@ -59,4 +61,11 @@ fun NavBar(
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun PreviewNavBar() {
+    val navController = rememberNavController()
+    NavBar(navController = navController)
 }
