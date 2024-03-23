273651ebc116079f1ade550be82798304f58926a
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 03:40:39 +0900

    ViewModel setup

--- app/src/main/java/com/example/okura/presentation/components/feed/NewsFeed.kt
+++ app/src/main/java/com/example/okura/presentation/components/feed/NewsFeed.kt
@@ -1,19 +1,20 @@
 package com.example.okura.presentation.components.feed
 
+import NewsViewModel
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.okura.domain.data.repository.newsList
-import com.example.okura.domain.model.NewsItem
 
 @Composable
 fun NewsFeed(
-    newsItems: List<NewsItem>,
+    newsViewModel: NewsViewModel,
     modifier: Modifier = Modifier,
 ) {
+    val newsItems by newsViewModel.articles.collectAsState() // ViewModelからニュースアイテムのリストを取得
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         modifier = modifier,
@@ -22,10 +23,4 @@ fun NewsFeed(
             NewsCard(item = newsItem)
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun PreviewNewsFeed() {
-    NewsFeed(newsList)
 }
