f0d6733ca63de0d0f6c82bca94e1b0c17095f29c
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 00:27:20 +0900

    Added new param

--- app/src/main/java/com/example/okura/domain/data/NewsRepository.kt
+++ app/src/main/java/com/example/okura/domain/data/NewsRepository.kt
@@ -1,10 +1,18 @@
 package com.example.okura.domain.data
+
+import com.example.okura.domain.model.NewsCategory
 import com.example.okura.domain.model.NewsItem
+
+fun getImageUrlForCategory(categoryName: String): String {
+    val category = NewsCategory.newsCategories.find { it.name == categoryName }
+    return category?.imageUrl ?: "デフォルトの画像URL"
+}
 
 val newsList =
     List(40) {
         NewsItem(
             id = it,
+            categoryId = it % 7 + 1,
             title = "$it",
             description = "$it",
             imageUrl = "https://picsum.photos/200/300?random=$it",
