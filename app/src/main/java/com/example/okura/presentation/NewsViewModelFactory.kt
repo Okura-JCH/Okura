f427346fb2bb6b7ec4eef895bf5759b60687a33d
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 03:40:59 +0900

    ViewModel Factory class setup

---
+++ app/src/main/java/com/example/okura/presentation/NewsViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.example.okura.presentation
+
+import NewsViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.okura.domain.data.repository.NewsRepository
+
+class NewsViewModelFactory(private val repository: NewsRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NewsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return NewsViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
