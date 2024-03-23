bbec393043aa81246c7d70a860038a58088f3eaa
Hina Nakahira <soleil.colza@icloud.com>
2024-03-23 17:39:42 +0900

    Avoid wildcard import

--- app/src/main/java/com/example/okura/presentation/components/HomeAppBar.kt
+++ app/src/main/java/com/example/okura/presentation/components/HomeAppBar.kt
@@ -4,7 +4,14 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.*
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
