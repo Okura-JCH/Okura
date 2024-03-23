d2c3221824eef52b68ebf1905e529ab711579f35
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 03:40:28 +0900

    ViewModel setup

--- app/src/main/java/com/example/okura/MainActivity.kt
+++ app/src/main/java/com/example/okura/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.okura
 
+import NewsViewModel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,11 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
+import com.example.okura.domain.data.repository.NewsRepository
+import com.example.okura.presentation.NewsViewModelFactory
 import com.example.okura.ui.theme.OkuraTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val newsRepository = NewsRepository()
+        val newsViewModel = ViewModelProvider(this, NewsViewModelFactory(newsRepository)).get(NewsViewModel::class.java)
         setContent {
             OkuraTheme {
                 Surface(
