8aae5086ac233afe647b283bce79a23f2508540d
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 00:26:24 +0900

    Added needed params

--- app/src/main/java/com/example/okura/domain/model/NewsItem.kt
+++ app/src/main/java/com/example/okura/domain/model/NewsItem.kt
@@ -2,7 +2,7 @@ package com.example.okura.domain.model
 
 data class NewsItem(
     val id: Int,
-    val category: NewsCategory,
+    val categoryId: Int,
     val imageUrl: String,
     val title: String,
     val description: String,
