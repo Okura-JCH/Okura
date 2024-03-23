48e8c3e7b115db6ec0500b0581162d93c976aa44
Hina Nakahira <soleil.colza@icloud.com>
2024-03-23 18:26:40 +0900

    Scaffold setup

--- app/src/main/java/com/example/okura/presentation/view/screens/PeopleScreen.kt
+++ app/src/main/java/com/example/okura/presentation/view/screens/PeopleScreen.kt
@@ -4,21 +4,59 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.okura.presentation.components.bars.HomeAppBar
+import com.example.okura.presentation.components.bars.NavigationBarItems
+import com.example.okura.ui.theme.OkuraTheme
 
 @Composable
 fun PeopleScreen(
-    navController: NavController,
+    navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
+    var selectedItem by remember { mutableIntStateOf(0) }
     Scaffold(
         modifier = modifier,
-    ) {
-            paddingValues ->
+        topBar = {
+            HomeAppBar(
+                title = { Text(text = "Okura") },
+            )
+        },
+        bottomBar = {
+            NavigationBarItems(
+                selectedItem = selectedItem,
+                onItemSelect = { index, route ->
+                    selectedItem = index
+                    navController.navigate(route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+            )
+        },
+    ) { paddingValues ->
         Text(
             text = "PeopleScreen",
             modifier = Modifier.padding(paddingValues),
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PeopleScreenPreview() {
+    OkuraTheme {
+        val navController = rememberNavController() // dummy navController for preview
+        PeopleScreen(navController = navController)
     }
 }
