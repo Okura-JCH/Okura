cd8f9ebb63bd90f7326bf63b4a6de4688e83d56c
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-03-24 03:57:26 +0000

    Refactor RetrofitInstance class

--- app/src/main/java/com/example/okura/domain/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/okura/domain/data/api/RetrofitInstance.kt
@@ -2,23 +2,26 @@ package com.example.okura.domain.data.api
 
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitClient {
-    private val logger: HttpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+val logger: HttpLoggingInterceptor =
+    HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
 
-    private val httpClient: OkHttpClient = OkHttpClient.Builder()
+val httpClient: OkHttpClient =
+    OkHttpClient.Builder()
         .addInterceptor(logger)
         .build()
 
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("http://10.155.68.250:3000/")
-        .client(httpClient)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    val api: NewsApiService by lazy {
-        retrofit.create(NewsApiService::class.java)
-    }
-}
+// object RetrofitInstance {
+//    private val retrofit by lazy {
+//        Retrofit.Builder()
+//            .baseUrl("http://10.155.68.250:3000/")
+//            .client(httpClient)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//    }
+// }
+//    val api: NewsApiService by lazy {
+//        retrofit.create(NewsApiService::class.java)
+//    }
+// }
