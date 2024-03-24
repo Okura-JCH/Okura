fb6b8cb54ab0c68441a56b040db066a934fff084
Hina Nakahira <soleil.colza@icloud.com>
2024-03-24 11:24:13 +0900

    Commenting out components related to Retrofit impl

--- app/src/main/java/com/example/okura/MainActivity.kt
+++ app/src/main/java/com/example/okura/MainActivity.kt
@@ -7,16 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModelProvider
-import com.example.okura.domain.data.repository.NewsRepository
-import com.example.okura.presentation.NewsViewModelFactory
 import com.example.okura.ui.theme.OkuraTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val newsRepository = NewsRepository()
-        val newsViewModel = ViewModelProvider(this, NewsViewModelFactory(newsRepository)).get(NewsViewModel::class.java)
+//        val newsRepository = NewsRepository()
+//        val newsViewModel = ViewModelProvider(this, NewsViewModelFactory(newsRepository)).get(NewsViewModel::class.java)
         setContent {
             OkuraTheme {
                 Surface(
